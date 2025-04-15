4ba1dd4502a15dbde7b669fbe31430dd69dde58f
DerrickPep <peprahderrick713@gmail.com>
2025-04-15 11:21:38 +0000

    excercise_1

---
+++ lesson1_print_basic_strings.py
@@ -0,0 +1,16 @@
+print("My name is Emmanuel Ahiaku")
+print("I am a descendant of Agorkoli")
+print("I come from Anloga Junction")
+print("My hobby is to curse people")
+print("My role model is Derrick")
+print("I look up to him a lot")
+
+print('I like a lot of girls')
+print('I even have gonorrhea')
+print("Derrick is the best")
+
+
+print("My name is", "Ahiaku", "and I am", 70, "years old")
+print("Ahiaku said, 'He's a big bitch!'")
+print('And Dorothy replied, "I totaly agree!"')
+print("GHANA @ 60 ")
